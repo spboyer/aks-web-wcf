f2fb8977cca1101372fc96db253d1d56efb43d40
Shayne Boyer <spboyer@live.com>
2018-02-02 11:11:51 -0500

    init

---
+++ ClientApp/src/app/people/people.component.ts
@@ -0,0 +1,25 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-people',
+  templateUrl: './people.component.html',
+  styleUrls: ['./people.component.css']
+})
+export class PeopleComponent {
+  public people: Person[];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<Person[]>(baseUrl + 'api/SampleData/people').subscribe(result => {
+      this.people = result;
+    }, error => console.error(error));
+  }
+}
+
+interface Person {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+  id: number;
+}
